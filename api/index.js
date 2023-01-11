0e81cca690205cfa8f3e09cbf3c4f9894b7e400c
Luiz Cláudio <danellaclaudioluiz@gmail.com>
2023-01-11 15:43:09 -0300

    added connection mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+    res.json("this is main url")
+    console.log("this is main url")
+})
 
 app.listen("5000", () => {
     console.log("Backend is running.")
